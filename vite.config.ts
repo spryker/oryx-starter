ab5ff28e0974ccce0a42e2300f32003caf246c82
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-03-16 09:53:53 +0200

    Updated dependencies version.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import {viteStaticCopy} from "vite-plugin-static-copy";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   root: './src',
