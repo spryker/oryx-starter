c2aa576d5d3978115b7775dc08b6bfe0cdfb3db7
Andrii Ievtukhov <andrii.ievtukhov@spryker.com>
2023-03-17 14:04:15 +0200

    add labs

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import { appBuilder } from '@spryker-oryx/core';
 import { b2cFeatures } from '@spryker-oryx/presets';
 import { storefrontTheme } from '@spryker-oryx/themes';
+import { labsFeature } from '../labs/src/feature';
 
 export const app = appBuilder()
   .withEnvironment(import.meta.env)
   .withFeature(b2cFeatures)
+  .withFeature(labsFeature)
   .withTheme(storefrontTheme)
   .create();
