1ad7b90898f6974ff7fd22a860a7a6be23489860
Alexander Kovalenko <alexander.kovalenko@spryker.com>
2023-07-20 17:41:40 +0300

    Fix

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,28 @@
-import { appBuilder } from '@spryker-oryx/application';
-import { storefrontFeatures } from '@spryker-oryx/presets/storefront';
+import {appBuilder, applicationFeature, SapiAuthComponentsFeature, SapiAuthFeature} from '@spryker-oryx/application';
 import { storefrontTheme } from '@spryker-oryx/themes';
+import {uiFeature} from "@spryker-oryx/ui";
+import {coreFeature} from "@spryker-oryx/core";
+import {RouterFeature} from "@spryker-oryx/router";
+import {I18nFeature} from "@spryker-oryx/i18n";
+import {cartFeature} from "@spryker-oryx/cart";
+import {checkoutFeature} from "@spryker-oryx/checkout";
+import {orderFeature} from "@spryker-oryx/order";
+import {contentFeature} from "@spryker-oryx/content";
+import {formFeature} from "@spryker-oryx/form";
+
+export const storefrontFeatures = [
+    uiFeature,
+    coreFeature,
+    new SapiAuthFeature(),
+    new SapiAuthComponentsFeature(),
+    new RouterFeature(),
+    new I18nFeature(),
+    cartFeature,
+    checkoutFeature,
+    orderFeature,
+    contentFeature,
+    formFeature,
+];
 
 export const app = appBuilder()
     .withFeature(storefrontFeatures)
