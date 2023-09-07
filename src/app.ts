19de6732c79905c638ab11edb8b24bcc1bb53dd5
Víctor Merino <victor.merino@jungheinrich.es>
2023-09-07 19:21:48 +0200

    Install latest version oryx packages, and the labs package. Also, use labs features, what removes background in images thanks to a cloudinary function

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import { appBuilder } from '@spryker-oryx/application';
 import { storefrontFeatures } from '@spryker-oryx/presets/storefront';
 import { storefrontTheme } from '@spryker-oryx/themes';
 import {colorPalette} from '@spryker-oryx/experience'
+import { labsFeatures } from '@spryker-oryx/labs';
 
 export const app = appBuilder()
     .withFeature(storefrontFeatures)
+    .withFeature(labsFeatures)
     .withTheme(storefrontTheme)
     .withTheme({
         designTokens: [{
