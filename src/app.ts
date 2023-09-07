d8939c302570163cd1a700c78518e795e675c293
Víctor Merino <victor.merino@jungheinrich.es>
2023-09-07 13:11:45 +0200

    Change primary color of the theme

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import { appBuilder } from '@spryker-oryx/application';
 import { storefrontFeatures } from '@spryker-oryx/presets/storefront';
 import { storefrontTheme } from '@spryker-oryx/themes';
+import {colorPalette} from '@spryker-oryx/experience'
 
 export const app = appBuilder()
     .withFeature(storefrontFeatures)
     .withTheme(storefrontTheme)
+    .withTheme({
+        designTokens: [{
+            color: {
+                primary: colorPalette.colors.lime
+            }
+        }]
+    })
     .withEnvironment(import.meta.env)
     .create();
