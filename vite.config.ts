4936967cf1b55e3a2273dd39bd8d8f0c58c4e1ec
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-09-23 20:20:41 +0200

    single quotes

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import dotenv from "dotenv";
-import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
+import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 dotenv.config();
 
 export default defineConfig({
-  root: "./src",
-  envDir: "../",
-  envPrefix: ["SCOS", "ORYX"],
+  root: './src',
+  envDir: '../',
+  envPrefix: ['SCOS', 'ORYX'],
   build: {
-    outDir: "../dist/client",
+    outDir: '../dist/client',
     emptyOutDir: true,
   },
   define: {
-    "import.meta.env.SCOS_BASE_URL": `"${
+    'import.meta.env.SCOS_BASE_URL': `"${
       process.env.SCOS_BASE_URL ?? process.env.ORYX_FALLBACK_SCOS_BASE_URL
     }"`,
-    __ORYX_FEATURE_VERSION__: `"${process.env.ORYX_FEATURE_VERSION ?? ""}"`,
+    __ORYX_FEATURE_VERSION__: `"${process.env.ORYX_FEATURE_VERSION ?? ''}"`,
   },
   server: {
     port: 3000,
@@ -25,8 +25,8 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: "../src/assets/addresses",
-          dest: "../../dist/client/assets",
+          src: '../src/assets/addresses',
+          dest: '../../dist/client/assets',
         },
       ],
     }),
