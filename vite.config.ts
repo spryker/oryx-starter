95df117766e65c118571d64e11d6ad83d23eef36
tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>
2023-10-06 11:05:43 +0200

    Oryx feature version (#31)
    Allow for feature version.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 dotenv.config();
 
 export default defineConfig({
   root: './src',
   envDir: '../',
-  envPrefix: ['SCOS', 'STORE', 'ORYX'],
+  envPrefix: ['SCOS', 'ORYX'],
   build: {
     outDir: '../dist/client',
     emptyOutDir: true,
   },
   define: {
-    'import.meta.env.SCOS_BASE_URL':`"${process.env.SCOS_BASE_URL ?? process.env.ORYX_FALLBACK_SCOS_BASE_URL}"`
+    'import.meta.env.SCOS_BASE_URL': `"${
+      process.env.SCOS_BASE_URL ?? process.env.ORYX_FALLBACK_SCOS_BASE_URL
+    }"`,
+    __ORYX_FEATURE_VERSION__: `"${process.env.ORYX_FEATURE_VERSION ?? ''}"`,
   },
   server: {
     port: 3000,
@@ -28,4 +31,4 @@ export default defineConfig({
       ],
     }),
   ],
-})
+});
