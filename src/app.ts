12248633f2a49d545a148347b1a7ea8622b3fb2a
Stan <dunqan@w9.pl>
2023-11-23 16:43:45 +0100

    fix: improve boilderplate (#39)
    Co-authored-by: tobi-or-not-tobi <tobi-or-not-tobi@users.noreply.github.com>

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-import { appBuilder } from '@spryker-oryx/application';
-import { fulfillmentFeatures } from '@spryker-oryx/presets/fulfillment';
-import { fulfillmentTheme } from '@spryker-oryx/themes';
-
+import { appBuilder } from "@spryker-oryx/application";
+import { fulfillmentFeatures } from "@spryker-oryx/presets/fulfillment";
+import { fulfillmentTheme } from "@spryker-oryx/themes";
+import { ColorMode } from "@spryker-oryx/utilities";
 const env = import.meta.env;
 
 appBuilder()
-    .withEnvironment(env)
-    .withFeature(fulfillmentFeatures({
-        picking: {
-            appVersion: env.ORYX_APP_VERSION,
-        },
-    }))
-    .withTheme(fulfillmentTheme)
-    .create();
+  .withEnvironment(env)
+  .withFeature(
+    fulfillmentFeatures({ picking: { appVersion: env.ORYX_APP_VERSION } })
+  )
+  .withOptions({ "oryx-app": { colorMode: ColorMode.Light } })
+  .withTheme(fulfillmentTheme)
+  .create();
